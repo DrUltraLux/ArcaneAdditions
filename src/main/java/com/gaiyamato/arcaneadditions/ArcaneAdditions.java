de7f6970ef554b12065edfdb8b30510a1541fceb
Gaiyamato <63906224+Gaiyamato@users.noreply.github.com>
2020-09-08 17:53:58 +1000

    Updating

--- src/main/java/com/gaiyamato/arcaneadditions/ArcaneAdditions.java
+++ src/main/java/com/gaiyamato/arcaneadditions/ArcaneAdditions.java
@@ -12,7 +12,7 @@ public class ArcaneAdditions
 {
     public static final String MODID = "arcaneadditions";
     public static final String NAME = "Arcane Additions";
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "GRADLE:VERSION";
 
     private static Logger logger;
 
