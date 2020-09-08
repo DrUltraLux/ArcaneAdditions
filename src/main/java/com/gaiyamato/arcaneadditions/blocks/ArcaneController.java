e40a70c603342862db2848fe6518a80cabf3d9b9
Gaiyamato <63906224+Gaiyamato@users.noreply.github.com>
2020-09-09 01:52:02 +1000

    Controller

---
+++ src/main/java/com/gaiyamato/arcaneadditions/blocks/ArcaneController.java
@@ -0,0 +1,13 @@
+package com.gaiyamato.arcaneadditions.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class ArcaneController extends Block {
+
+    public ArcaneController(Material materialIn) {
+        super(materialIn);
+        // TODO Auto-generated constructor stub
+    }
+    
+}
