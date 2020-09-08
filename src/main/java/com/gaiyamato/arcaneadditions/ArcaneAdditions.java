c174634d461294314620fe8b1efdadaf34ea526f
Gaiyamato <63906224+Gaiyamato@users.noreply.github.com>
2020-09-08 23:01:02 +1000

    Updates

--- src/main/java/com/gaiyamato/arcaneadditions/ArcaneAdditions.java
+++ src/main/java/com/gaiyamato/arcaneadditions/ArcaneAdditions.java
@@ -1,6 +1,5 @@
 package com.gaiyamato.arcaneadditions;
 
-import net.minecraft.block.material.Material;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.Item.ToolMaterial;
