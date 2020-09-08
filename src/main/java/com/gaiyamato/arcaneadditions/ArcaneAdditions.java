db5df7cd0817cd26a891393f83edca464ea7989e
Gaiyamato <63906224+Gaiyamato@users.noreply.github.com>
2020-09-08 18:14:01 +1000

    Updates

--- src/main/java/com/gaiyamato/arcaneadditions/ArcaneAdditions.java
+++ src/main/java/com/gaiyamato/arcaneadditions/ArcaneAdditions.java
@@ -1,14 +1,13 @@
 package com.gaiyamato.arcaneadditions;
 
-
-//import net.minecraft.init.Blocks;
+import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
-//import net.minecraftforge.fml.common.Mod.EventHandler;
-//import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-//import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-//import net.minecraftforge.fml.common.SidedProxy;
-//import net.minecraftforge.fml.common.event.*;
-//import org.apache.logging.log4j.Logger;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.*;
+import org.apache.logging.log4j.Logger;
 
 @Mod(modid = ArcaneAdditions.MODID, name = ArcaneAdditions.NAME, version = ArcaneAdditions.VERSION)
 public class ArcaneAdditions
@@ -17,5 +16,18 @@ public class ArcaneAdditions
     public static final String NAME = "Arcane Additions";
     public static final String VERSION = "GRADLE:VERSION";
 
-    //private static Logger logger;
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
 }
