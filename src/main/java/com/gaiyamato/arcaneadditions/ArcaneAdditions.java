e6a20577f2120329800a831c8442be3a41d31177
Gaiyamato <63906224+Gaiyamato@users.noreply.github.com>
2020-09-08 17:56:59 +1000

    Updating Structure

--- src/main/java/com/gaiyamato/arcaneadditions/ArcaneAdditions.java
+++ src/main/java/com/gaiyamato/arcaneadditions/ArcaneAdditions.java
@@ -5,6 +5,8 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.*;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = ArcaneAdditions.MODID, name = ArcaneAdditions.NAME, version = ArcaneAdditions.VERSION)
@@ -15,17 +17,41 @@ public class ArcaneAdditions
     public static final String VERSION = "GRADLE:VERSION";
 
     private static Logger logger;
+    @SidedProxy(clientSide = "com.gaiyamato.arcaneadditions.proxy.ClientProxy", serverSide = "com.gaiyamato.arcaneadditions.proxy.CommonProxy")
+	public static CommonProxy proxy;
 
     @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
-        logger = event.getModLog();
-    }
+	public static void preInit (FMLPreInitializationEvent event) {
+		proxy.preInit(event);
+		configDirectory = event.getModConfigurationDirectory();
+	}
 
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-        // some example code
-        //logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
-    }
+	@EventHandler
+	public static void init (FMLInitializationEvent event) {
+		proxy.init(event);
+	}
+
+	@EventHandler
+	public static void postInit (FMLPostInitializationEvent event) {
+		proxy.postInit(event);
+	}
+
+	@EventHandler
+	public static void serverStarting (FMLServerStartingEvent event) {
+		proxy.serverStarting(event);
+	}
+
+	@EventHandler
+	public static void serverStarted (FMLServerStartedEvent event) {
+		proxy.serverStarted(event);
+	}
+
+	@EventHandler
+	public static void loadComplete (FMLLoadCompleteEvent event) {
+		proxy.loadComplete(event);
+	}
+
+	public static ResourceLocation location (String string) {
+		return new ResourceLocation(MODID, string);
+	}
 }
