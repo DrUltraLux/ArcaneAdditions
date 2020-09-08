9160a2dfa421ec3c25870cca8980727fe423ea19
Gaiyamato <63906224+Gaiyamato@users.noreply.github.com>
2020-09-08 18:18:20 +1000

    Imports

--- src/main/java/com/gaiyamato/arcaneadditions/ArcaneAdditions.java
+++ src/main/java/com/gaiyamato/arcaneadditions/ArcaneAdditions.java
@@ -1,5 +1,7 @@
 package com.gaiyamato.arcaneadditions;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
