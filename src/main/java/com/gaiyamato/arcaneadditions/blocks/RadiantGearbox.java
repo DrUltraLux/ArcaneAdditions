18d685309fb14b96920f3994c544d1e85473badb
Gaiyamato <63906224+Gaiyamato@users.noreply.github.com>
2020-09-09 01:49:58 +1000

    Gearbox

---
+++ src/main/java/com/gaiyamato/arcaneadditions/blocks/RadiantGearbox.java
@@ -0,0 +1,11 @@
+package com.gaiyamato.arcaneadditions.blocks;
+
+import mysticalmechanics.block.BlockGearbox;
+import net.minecraft.block.material.Material;
+
+public class RadiantGearbox extends BlockGearbox
+{
+    public RadiantGearbox(final Material material) {
+        super(material);
+    }
+}
