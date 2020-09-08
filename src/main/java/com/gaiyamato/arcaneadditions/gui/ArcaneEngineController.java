48c1ea5c3dc24cd11eedd30e11215df82d665d5c
Gaiyamato <63906224+Gaiyamato@users.noreply.github.com>
2020-09-09 02:00:57 +1000

    Engine GUI

--- src/main/java/com/gaiyamato/arcaneadditions/gui/ArcaneEngineController.java
+++ src/main/java/com/gaiyamato/arcaneadditions/gui/ArcaneEngineController.java
@@ -4,9 +4,15 @@ import com.gaiyamato.arcaneadditions.ArcaneAdditions;
 import com.gaiyamato.arcaneadditions.blocks.ArcaneController;
 
 import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.inventory.Container;
 import net.minecraft.util.ResourceLocation;
 
 public class ArcaneEngineController extends GuiContainer {
+    public ArcaneEngineController(Container inventorySlotsIn) {
+        super(inventorySlotsIn);
+        // TODO Auto-generated constructor stub
+    }
+
     ArcaneController engineControl;
     private ResourceLocation guiTexture = new ResourceLocation(ArcaneAdditions.MODID, "textures/gui/engine/engine_gui.png");
 
