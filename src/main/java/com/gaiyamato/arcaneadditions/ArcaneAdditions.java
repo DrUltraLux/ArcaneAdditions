e2ce304d8606023603b437ea345b0c42c549c6a0
Gaiyamato <63906224+Gaiyamato@users.noreply.github.com>
2020-09-08 17:44:41 +1000

    Initial Setup

--- src/main/java/com/gaiyamato/arcaneadditions/ArcaneAdditions.java
+++ src/main/java/com/gaiyamato/arcaneadditions/ArcaneAdditions.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.gaiyamato.arcaneadditions;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,12 +7,12 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = ArcaneAdditions.MODID, name = ArcaneAdditions.NAME, version = ArcaneAdditions.VERSION)
+public class ArcaneAdditions
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "arcaneadditions";
+    public static final String NAME = "Arcane Additions";
+    public static final String VERSION = "0.0.1";
 
     private static Logger logger;
 
