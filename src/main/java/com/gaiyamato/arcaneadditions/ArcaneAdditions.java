279353daeffbed3fd1f29d977c7a37dfa2bca021
Gaiyamato <63906224+Gaiyamato@users.noreply.github.com>
2020-09-08 18:00:03 +1000

    Removing unused for now

--- src/main/java/com/gaiyamato/arcaneadditions/ArcaneAdditions.java
+++ src/main/java/com/gaiyamato/arcaneadditions/ArcaneAdditions.java
@@ -1,5 +1,6 @@
 package com.gaiyamato.arcaneadditions;
 
+/*
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -7,6 +8,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.*;
+*/
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = ArcaneAdditions.MODID, name = ArcaneAdditions.NAME, version = ArcaneAdditions.VERSION)
